63a2329f76f0c0c28737a338dddb3671bec8f26c
StefanieAviles <stefanie42019@gmail.com>
2022-03-15 13:28:04 -0500

    Implementacion de funciones hide password y show password

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,42 +1,6 @@
 // aqui exportaras las funciones que necesites
 
 export const myFunction = () => {
-  // aqui tu codigo
-  //console.log('Hola mundo!');
+  /* aqui tu codigo
+  console.log('Hola mundo!'); */
 };
-
-const register = document.getElementById("register");
-register.addEventListener("click", () => {
-  const pageTwo= document.getElementById("containerPageTwo");
-  const pageOne= document.getElementById("containerPageOne");
-  pageOne.style.display="none";
-  pageTwo.style.display="";
-});
-
-const eyeLogo= document.querySelector(".fa-eye");
-const eyeSlashLogo= document.querySelector(".fa-eye-slash");
-const password= document.querySelector("#password");
-eyeLogo.addEventListener("click", () => {
-  eyeLogo.style.display="none";
-  eyeSlashLogo.style.display="";
-  password.type= "text";
-});
-eyeSlashLogo.addEventListener("click", () => {
-  eyeLogo.style.display="";
-  eyeSlashLogo.style.display="none";
-  password.type= "password";
-});
-const eyeLogo2= document.querySelector("#eyeLogo");
-const eyeSlashLogo2= document.querySelector("#eyeSlashLogo");
-const password2= document.querySelector("#newUserPassword");
-eyeLogo2.addEventListener("click", () => {
-  eyeLogo2.style.display="none";
-  eyeSlashLogo2.style.display="";
-  password2.type= "text";
-});
-eyeSlashLogo2.addEventListener("click", () => {
-  eyeLogo2.style.display="";
-  eyeSlashLogo2.style.display="none";
-  password2.type= "password";
-});
-
--- src/main.js
+++ src/main.js
@@ -3,3 +3,38 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const register = document.getElementById('register');
+register.addEventListener('click', () => {
+  const pageTwo = document.getElementById('containerPageTwo');
+  const pageOne = document.getElementById('containerPageOne');
+  pageOne.style.display = 'none';
+  pageTwo.style.display = '';
+});
+
+// eslint-disable-next-line camelcase
+function show_password(id1, id2, id3) {
+  const eye = document.getElementById(id1);
+  const eyeSlash = document.getElementById(id2);
+  const password = document.getElementById(id3);
+  eye.addEventListener('click', () => {
+    eye.style.display = 'none';
+    eyeSlash.style.display = '';
+    password.setAttribute('type', 'text');
+  });
+}
+// eslint-disable-next-line camelcase
+function hide_password(id1, id2, id3) {
+  const eye = document.getElementById(id1);
+  const eyeSlash = document.getElementById(id2);
+  const password = document.getElementById(id3);
+  eyeSlash.addEventListener('click', () => {
+    eye.style.display = '';
+    eyeSlash.style.display = 'none';
+    password.setAttribute('type', 'password');
+  });
+}
+show_password('eyeLogo1', 'eyeSlashLogo1', 'password');
+hide_password('eyeLogo1', 'eyeSlashLogo1', 'password');
+show_password('eyeLogo2', 'eyeSlashLogo2', 'newUserPassword');
+hide_password('eyeLogo2', 'eyeSlashLogo2', 'newUserPassword');
