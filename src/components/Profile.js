73117879fb246439d7919aa9227c67f827da6721
StefanieAviles <stefanie42019@gmail.com>
2022-03-23 17:48:03 -0500

    Actualizar posts en pantalla

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 export const Profile = () => {
-    const containerProfile = `<header>
+  const errorMessage = '<p id="message"></p>';
+  const containerProfile = `<header>
                              <nav>
                               <ul>
                                <li><i id="Homeicon" class="fa fa-home"> </i></li>
@@ -11,10 +12,12 @@ export const Profile = () => {
                             </header>
                             <div id="profileInfo">PERFIL</div>
                             <form id="createPost">
+                             <p id="messagePost"></p>
                              <input id="title" placeholder="Titulo">
                              <textarea id="post" placeholder="Descripcion"></textarea>
                              <button id="postButton">Publicar</button>
                             </form>
                             <div id="postsContainer"></div>`;
-  return containerProfile;
+  const profileDivs = errorMessage + containerProfile;
+  return profileDivs;
 };
