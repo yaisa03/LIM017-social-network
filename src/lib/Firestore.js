16371117609dcd7bda358f53a216edcff86a0121
yaisa03 <yaisamichellea@gmail.com>
2022-04-21 14:46:39 -0500

    reestructuracion de Firestore.js final

--- src/lib/Firestore.js
+++ src/lib/Firestore.js
@@ -5,9 +5,6 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-console */
-/* import {
-  getStorage, ref, uploadBytes, getDownloadURL,
-} from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-storage.js'; */
 import {
   getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail,
   GoogleAuthProvider, signInWithPopup, sendEmailVerification, signOut, updateProfile,
@@ -16,13 +13,8 @@ import {
   ref, uploadBytes, getDownloadURL, deleteUser, getDocs,
 } from './FirebaseImport.js';
 import { app } from './FirebaseInit.js';
-import { ShowPosts } from '../components/ShowPosts.js';
 import {
-  /* emailMessageVerificacionOK,  cleanMessageError, validateEmailVerification,
-  removeMessageError, addMessage,
-resetPasswordMessageOK, removeMessage, addMessageError, messageErrorCases,
-  getFileChoosenProfile, getReferenceImg, getFileChoosenPost, */showUserPostsById,
-  showPostsHome, putLikesPosts, // getFileChoosenPostEdit, emailVerificacionMessage,
+  showUserPostsById, showPostsByType, showPostsHome,
 } from './index.js';
 
 const db = getFirestore(app);
@@ -62,12 +54,12 @@ export function passwordReset(email) {
 // eslint-disable-next-line consistent-return
 export function setUser(displayName, photoURL) {
   const user = auth.currentUser;
+  const uid = user.uid;
   if (user !== null) {
     user.displayName = displayName;
     user.photoURL = photoURL;
-    const uid = user.uid;
-    return uid;
   }
+  return uid;
 }
 // eslint-disable-next-line consistent-return
 export function setUserPhoto(photoUserURL) {
@@ -89,7 +81,7 @@ export async function updatePhotoPosts(photoUserURL, user) {
 }
 // funcion que permite editar el nombre de usurio
 export function setUserInfo(newname) {
-  updateProfile(auth.currentUser, {
+  return updateProfile(auth.currentUser, {
     displayName: newname,
   }).then(() => {
     updateNamePosts(newname, auth.currentUser);
@@ -99,14 +91,14 @@ export function setUserInfo(newname) {
 export async function updateNamePosts(newname, user) {
   const q = query(collection(db, 'posts'), where('UserId', '==', user.uid));
   const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((post) => {
+  return querySnapshot.forEach((post) => {
     setDoc(doc(db, 'posts', post.id), { userName: newname }, { merge: true });
   });
 }
 // funcion que permite eliminar el usuario
 export function deleteAccount() {
   const user = auth.currentUser;
-  deleteUser(user).then(() => {
+  return deleteUser(user).then(() => {
     deleteUserPosts(user);
   }).catch((error) => error);
 }
@@ -114,8 +106,7 @@ export function deleteAccount() {
 export async function deleteUserPosts(user) {
   const q = query(collection(db, 'posts'), where('UserId', '==', user.uid));
   const querySnapshot = await getDocs(q);
-  console.log(querySnapshot);
-  querySnapshot.forEach((post) => {
+  return querySnapshot.forEach((post) => {
     deleteDoc(doc(db, 'posts', post.id));
   });
 }
@@ -136,18 +127,11 @@ export async function updatePostImage(id, title, post, url) {
   setDoc(doc(db, 'posts', id), { content: post }, { merge: true });
   setDoc(doc(db, 'posts', id), { image: url }, { merge: true });
 }
-// funcion para obtener la informacion del usuario actual
-export function getUser() {
-  // const auth = getAuth();
-  const user = auth.currentUser;
-  return user;
-}
 // Funcion que guarda post con foto en firebase
 export function uploadPostImage(title, post, url, storageName, level, type) {
-  // const auth = getAuth();
   const user = auth.currentUser;
   console.log(url);
-  addDoc(collection(db, 'posts'), {
+  return addDoc(collection(db, 'posts'), {
     UserId: user.uid,
     postTitle: title,
     content: post,
@@ -160,81 +144,32 @@ export function uploadPostImage(title, post, url, storageName, level, type) {
     postLevel: level,
     postType: type,
   });
-  findPostById();
 }
-// Funcion que guarda post sin foto en firebase
-/* export function uploadPost(title, post, level, type) {
-  // const auth = getAuth();
-  const user = auth.currentUser;
-  addDoc(collection(db, 'posts'), {
-    UserId: user.uid,
-    postTitle: title,
-    content: post,
-    date: new Date(),
-    likes: [],
-    image: '',
-    storageImage: '',
-    userImage: user.photoURL,
-    userName: user.displayName,
-    postLevel: level,
-    postType: type,
-  });
-} */
 // Funcion que muestra los posts del usuario dueno del perfil
 export async function findPostById() {
   // const auth = getAuth();
   const user = auth.currentUser;
   const postsRef = collection(db, 'posts');
   const q = query(postsRef, where('UserId', '==', user.uid), orderBy('date', 'desc'));
-  onSnapshot(q, (snapshot) => {
+  return onSnapshot(q, (snapshot) => {
     showUserPostsById(snapshot);
-    /* const containerPosts = document.getElementById('postsContainer');
-    containerPosts.innerHTML = '';
-    snapshot.forEach((e) => {
-      containerPosts.innerHTML += ShowPostsById(e, e.data());
-      // if (e.data().image === '') {
-      //  document.getElementById('uploadPostImages').style.display = 'none';
-      // }
-      const button = document.querySelectorAll('.editButton');
-      const text = document.querySelectorAll(`.${button.id}`);
-      text.forEach((el) => {
-        el.readonly = true;
-      });
-      editPosts();
-      deletePosts();
-    }); */
   });
 }
 // Funcion que busca todos los posts en la app
 export async function findPosts() {
   const q = query(collection(db, 'posts'), orderBy('date', 'desc'));
-  onSnapshot(q, (querySnapshot) => {
+  return onSnapshot(q, (querySnapshot) => {
     showPostsHome(querySnapshot);
-    /* const containerPosts = document.getElementById('postsContainer');
-    containerPosts.innerHTML = '';
-    let createdPosts = '';
-    querySnapshot.forEach((d) => {
-      createdPosts += ShowPosts(d, d.data());
-      containerPosts.innerHTML = createdPosts;
-      // if (d.data().image === '') {
-      //  document.getElementById('uploadPostImages').style.display = 'none';
-      // }
-      // console.log(d.data());
-    });
-    AddLikes(); */
-    // search(createdPosts);
   });
 }
-/* function filterPost(arrayPosts, condition) {
-  console.log(JSON.parse(arrayPosts).filter((post) => (post.postTitle).includes(condition)));
-  // return;
-} */
-/* export function search(createdPost) {
-  const searchPost = document.getElementById('searchPost');
-  searchPost.addEventListener('keyup', () => {
-    filterPost(createdPost, searchPost.value);
+// funcion que permite obtener los por el tipo de receta
+export async function findPostByType(type) {
+  const postsRef = collection(db, 'posts');
+  const q = query(postsRef, where('postType', '==', type), orderBy('date', 'desc'));
+  onSnapshot(q, (snapshot) => {
+    showPostsByType(snapshot);
   });
-} */
+}
 // Funcion que elimina los posts de la base de datos de firebase
 export function postDeleted(id) {
   deleteDoc(doc(db, 'posts', id));
@@ -253,52 +188,8 @@ export async function getArrayLikes(e) {
   let array = docSnap.data().likes;
   return array;
 }
-export function AddLikes() {
-  // const auth = getAuth();
-  const user = auth.currentUser;
-  putLikesPosts(user);
-  /* const likeButton = document.querySelectorAll('.likeButton');
-  likeButton.forEach((e) => {
-    e.addEventListener('click', async () => {
-      let arrayLikes = await getArrayLikes(e.id);
-      let count = 0;
-      const arrayCounter = arrayLikes.length;
-      for (let i = 0; i < arrayLikes.length; i++) {
-        if (arrayLikes[i] === user.uid) {
-          arrayLikes.splice(i, 1);
-          postLike(e.id, arrayLikes);
-          break;
-        } else {
-          // eslint-disable-next-line no-unused-vars
-          count++;
-        }
-      }
-      if (count === arrayCounter) {
-        arrayLikes.push(user.uid);
-        postLike(e.id, arrayLikes);
-      }
-    });
-  }); */
-}
 // Funcion que permite cerrar sesion de un usuario
 export function SignOut() {
-  // const auth = getAuth();
-  signOut(auth).then(() => {
-    // Sign-out successful.
-    console.log('Sesion cerrada satisfactoriamente');
-  }).catch((/* error */) => {
-    // An error happened.
-  });
-}
-// funcion que permite obtener los por Postres
-export async function findPostByType(type) {
-  const postsRef = collection(db, 'posts');
-  const q = query(postsRef, where('postType', '==', type), orderBy('date', 'desc'));
-  onSnapshot(q, (snapshot) => {
-    const containerPosts = document.getElementById('postsContainer');
-    containerPosts.innerHTML = '';
-    snapshot.forEach((e) => {
-      containerPosts.innerHTML += ShowPosts(e, e.data());
-    });
-  });
+  return signOut(auth).then(() => {
+  }).catch((error) => error);
 }
